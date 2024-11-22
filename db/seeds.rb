f86bf4426fd80e303c5114f58918c666e3648f2c
murichristopher <murichristopher@outlook.com>
2024-11-22 17:14:09 -0300

    Update database.yml and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,55 +1,55 @@
-## Cleaning all entities
-Task.delete_all
-User.delete_all
+# ## Cleaning all entities
+# Task.delete_all
+# User.delete_all
 
-## Criar users
+# ## Criar users
 
-murileonsio = User.create!(
-  email: "murileonsio@gmail.com",
-  name: "Murileonsio",
-)
+# murileonsio = User.create!(
+#   email: "murileonsio@gmail.com",
+#   name: "Murileonsio",
+# )
 
-alveta = User.create!(
-  email: "alveta@gmail.com",
-  name: "Alveta",
-)
+# alveta = User.create!(
+#   email: "alveta@gmail.com",
+#   name: "Alveta",
+# )
 
-### Tasks 
+# ### Tasks 
 
-task1 = Task.create!(
-  title: "Comprar leite",
-  coins: 2,
-  user: murileonsio
-)
-task2 = Task.create!(
-  title: "Comprar pao",
-  coins: 3,
-  user: alveta
-)
-Task.create!(
-  title: "Comprar açucar",
-  coins: 2
-)
-Task.create!(
-  title: "Comprar pêra",
-  coins: 7
-)
-Task.create!(
-  title: "Comprar amendoa",
-  coins: 5
-)
-Task.create!(
-  title: "Comprar lentilha",
-  coins: 3
-)
-
-
+# task1 = Task.create!(
+#   title: "Comprar leite",
+#   coins: 2,
+#   user: murileonsio
+# )
+# task2 = Task.create!(
+#   title: "Comprar pao",
+#   coins: 3,
+#   user: alveta
+# )
+# Task.create!(
+#   title: "Comprar açucar",
+#   coins: 2
+# )
+# Task.create!(
+#   title: "Comprar pêra",
+#   coins: 7
+# )
+# Task.create!(
+#   title: "Comprar amendoa",
+#   coins: 5
+# )
+# Task.create!(
+#   title: "Comprar lentilha",
+#   coins: 3
+# )
 
 
-## Creating users coins
 
-murileonsio.coins = 2205
-alveta.coins = 1803
 
-murileonsio.save
-alveta.save
+# ## Creating users coins
+
+# murileonsio.coins = 2205
+# alveta.coins = 1803
+
+# murileonsio.save
+# alveta.save
